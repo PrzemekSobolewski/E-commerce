332a93118aa9eb0ed7a949bc5206a54a45ee9493
PrzemekSobolewski <przemus212@gmail.com>
2021-08-09 21:31:40 +0200

    configure linter and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "root": true,
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+      "@typescript-eslint",
+      "prettier"
+    ],
+    "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier"
+    ],
+    "rules": {
+        "no-console": 1,       
+        "prettier/prettier": 2 
+    }
+};
